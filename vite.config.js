e795c725d86dc306a8b9b8646a5cbfd1b5db43ea
MrKingst3r <trent.david98@gmail.com>
2024-08-09 21:00:32 -0500

    Updated config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const path = require('path')
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
-  }
+  },
+  base: './', // This is equivalent to `publicPath` in Vue CLI
+  build: {
+    outDir: 'dist', // This is equivalent to `outputDir`
+    assetsDir: 'static', // This is equivalent to `assetsDir`
+  },
 })
